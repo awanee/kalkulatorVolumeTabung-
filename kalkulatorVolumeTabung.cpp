da92d2c5487dabd37a3a2e57c845798c86193139
Avvun Erune <awan.cahya07@gmail.com>
2025-03-08 20:05:08 +0700

    udah gitu aja

---
+++ kalkulatorVolumeTabung.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "+--------------------------+\n"; // judul program;
+    cout << "| Kalkulator Volume Tabung |\n";
+    cout << "+--------------------------+\n";
+
+    const float phi = 3.14; // deklarasi variabel untuk rumus; 
+    int jari, tinggi;
+    float volume;
+
+    cout << "Masukkan Nilai Jari-Jari: "; cin >> jari; // meminta user untuk menginputkan nilai; 
+    cout << "Masukkan Nilai Tinggi Tabung; "; cin >> tinggi;
+
+    volume = phi * (jari * jari) * tinggi; // rumus ditulis setelah program meminta user untuk menginputkan nilai;
+
+    cout << "Volume Tabung: " << volume << endl; // menampilkan hasil    
+}
